3a4d6f93d12d7cf0e490a8f44e481e7215f8b328
MarcinMG <mmgrabowski@o2.pl>
2018-03-18 23:54:07 +0100

    Zad18.2

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,12 +1,9 @@
 package com.crud.tasks;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.validation.constraints.AssertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,11 +11,6 @@ public class TasksApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
-
-	@Test
-	public void testTest() {
-		Assert.assertTrue(false);
 	}
 
 }
