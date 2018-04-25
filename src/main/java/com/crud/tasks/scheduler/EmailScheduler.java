250233087d5b757d5f1f48d1cde4a8ca002fa48a
MarcinMG <mmgrabowski@o2.pl>
2018-04-26 01:03:20 +0200

    Zad24.3

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -3,6 +3,7 @@ package com.crud.tasks.scheduler;
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.MailCreatorService;
 import com.crud.tasks.service.SimpleEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,23 +18,40 @@ public class EmailScheduler {
     private SimpleEmailService simpleEmailService;
 
     @Autowired
+    private MailCreatorService mailCreatorService;
+
+    @Autowired
     private TaskRepository taskRepository;
 
     @Autowired
     private AdminConfig adminConfig;
 
+    private String message;
+
    // @Scheduled(cron = "0 0 10 * * *")
     @Scheduled(fixedDelay = 1000000)
     public void sendInformationEmail() {
-        long size = taskRepository.count();
-        String taskOrTasks = " tasks";
-        if(size == 1) {
-            taskOrTasks = " task";
-        }
+        hasTasks();
         simpleEmailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
-                "Currently in database you got: " + size + taskOrTasks)
-        );
+                message
+        ));
+    }
+
+    public boolean hasTasks() {
+        long size = taskRepository.count();
+        boolean tasksExist;
+        if (size == 0) {
+            message = "There is no task";
+            tasksExist = false;
+        } else if (size == 1) {
+            message = "You have " + size + " task";
+            tasksExist = true;
+        } else {
+            message = "You have " + size + " tasks";
+            tasksExist = true;
+        }
+        return tasksExist;
     }
 }
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,11 +1,15 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.scheduler.EmailScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MailCreatorService {
@@ -14,10 +18,19 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    private EmailScheduler emailScheduler;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context  = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
@@ -25,6 +38,28 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("company_name", adminConfig.getCompanyName());
         context.setVariable("company_goal", adminConfig.getCompanyGoal());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildSchedulerInformationEmail(String message) {
+
+        List<String> functionalityScheduler = new ArrayList<>();
+        functionalityScheduler.add("Sending update every 24 hours");
+        functionalityScheduler.add("Provides number of tasks");
+
+        Context contextScheduler = new Context();
+        contextScheduler.setVariable("message", message);
+        contextScheduler.setVariable("button", "Visit website");
+        contextScheduler.setVariable("goodbye", "See you in 24 hours");
+        contextScheduler.setVariable("show_button", false);
+        contextScheduler.setVariable("is_friend", true);
+        contextScheduler.setVariable("admin_config", adminConfig);
+        contextScheduler.setVariable("has_tasks", emailScheduler.hasTasks());
+        contextScheduler.setVariable("scheduler_functionality", functionalityScheduler);
+        return templateEngine.process("mail/scheduled-information-mail", contextScheduler);
     }
 }
